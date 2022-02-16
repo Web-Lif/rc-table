4eb08d0d1ad438143c36077678de5a044dd51840
robothot <854363956@qq.com>
2022-02-16 10:53:09 +0800

    fix: 修复表格的 Cell 单元格的样式问题

--- src/Cell.tsx
+++ src/Cell.tsx
@@ -6,7 +6,6 @@ const CellStyled = styled.div`
     display: inline-block;
     border-right: 1px solid var(--rc-table-border-color, #ddd);
     border-bottom: 1px solid var(--rc-table-border-color, #ddd);
-    padding: 0 8px;
     text-overflow: ellipsis;
     height: 100%;
     line-height: var(--rc-table-row-height);
